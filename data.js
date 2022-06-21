27b1d541f74b300dd4072b4ca892378ca5445119
krquinton <nscrexgst.dotr@gmail.com>
2022-06-21 12:04:35 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,291 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-corner-1",
+      "name": "Corner 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2193876640099397,
+        "pitch": 0.6924328562617514,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7326001964940563,
+          "pitch": 0.10587675292171816,
+          "rotation": 0,
+          "target": "1-corner-2"
+        },
+        {
+          "yaw": 0.1575660380805779,
+          "pitch": 0.04514617694374934,
+          "rotation": 0,
+          "target": "2-corner-3"
+        },
+        {
+          "yaw": 0.9255661922202236,
+          "pitch": 0.07879052982448798,
+          "rotation": 0,
+          "target": "3-corner-4"
+        },
+        {
+          "yaw": -0.9120878611190992,
+          "pitch": 0.691581565669166,
+          "rotation": 0,
+          "target": "4-ground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-corner-2",
+      "name": "Corner 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.19953599753019446,
+        "pitch": 0.6066897810445759,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3701070178952435,
+          "pitch": 0.1423026880710978,
+          "rotation": 0,
+          "target": "0-corner-1"
+        },
+        {
+          "yaw": -0.4251934672777544,
+          "pitch": 0.10570354594329956,
+          "rotation": 0,
+          "target": "3-corner-4"
+        },
+        {
+          "yaw": -1.119166186535617,
+          "pitch": 0.1979093488816588,
+          "rotation": 0,
+          "target": "2-corner-3"
+        },
+        {
+          "yaw": 0.5536955261395384,
+          "pitch": 0.5190259429776063,
+          "rotation": 0,
+          "target": "4-ground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corner-3",
+      "name": "Corner 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0067130868565907775,
+        "pitch": 0.7728527950845923,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8764268827069284,
+          "pitch": 0.21849298235300907,
+          "rotation": 0,
+          "target": "1-corner-2"
+        },
+        {
+          "yaw": 0.08000890751738687,
+          "pitch": 0.14137808841846322,
+          "rotation": 0,
+          "target": "0-corner-1"
+        },
+        {
+          "yaw": -0.662555034813133,
+          "pitch": 0.2254923566330973,
+          "rotation": 0,
+          "target": "3-corner-4"
+        },
+        {
+          "yaw": 0.5478545598959812,
+          "pitch": 0.2383077324125633,
+          "rotation": 0,
+          "target": "4-ground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-corner-4",
+      "name": "Corner 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.15556454536706354,
+        "pitch": 0.7493860883150418,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8747119404740591,
+          "pitch": 0.19465763895203736,
+          "rotation": 0,
+          "target": "2-corner-3"
+        },
+        {
+          "yaw": 0.009080327798718812,
+          "pitch": 0.14197791605695187,
+          "rotation": 0,
+          "target": "1-corner-2"
+        },
+        {
+          "yaw": -0.7641419873083759,
+          "pitch": 0.22276949564834148,
+          "rotation": 0,
+          "target": "0-corner-1"
+        },
+        {
+          "yaw": -0.4028149830282821,
+          "pitch": 0.25063294804814795,
+          "rotation": 0,
+          "target": "4-ground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ground",
+      "name": "Ground",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.04195899910462231,
+        "pitch": -0.005458400904748473,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.409122823376503,
+          "pitch": -0.18143905785007775,
+          "rotation": 0,
+          "target": "1-corner-2"
+        },
+        {
+          "yaw": -0.4464175283060001,
+          "pitch": -0.16236963411133942,
+          "rotation": 0,
+          "target": "2-corner-3"
+        },
+        {
+          "yaw": 0.5657403008989998,
+          "pitch": -0.1496858542139634,
+          "rotation": 0,
+          "target": "3-corner-4"
+        },
+        {
+          "yaw": 1.5412473993190172,
+          "pitch": -0.1968557931096342,
+          "rotation": 0,
+          "target": "0-corner-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Indang, Cavite",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
